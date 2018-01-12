35fbc208ab650e9b7ee097832cb2dd7f06ae8e89
Eric Wolfe <nuclearcat1337@gmail.com>
2018-01-11 16:00:18 -0800

    Initial commit for Alec Thotron's class

---
+++ project1.py
@@ -0,0 +1,17 @@
+
+
+def digInTheDirt() -> None:
+
+    inputLine = None
+    while (True):
+        inputLine = input()
+        if len (inputLine) < 3:
+            error()
+            continue
+        firstArgument = inputLine[0:1]
+
+def error() -> None:
+    print('ERROR')
+
+if __name__ == '__main__':
+    digInTheDirt()
