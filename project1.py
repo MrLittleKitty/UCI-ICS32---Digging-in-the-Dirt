cc5a86909395eb046dfddfaa7861563bc42aabcd
Eric Wolfe <nuclearcat1337@gmail.com>
2018-01-18 09:03:46 -0800

    All the live long day

--- project1.py
+++ project1.py
@@ -1,10 +1,26 @@
 from pathlib import *
 
-def digInTheDirt() -> None:
+def error() -> None:
+    print('ERROR')
 
-    inputLine = None
-    mode = None
-    directoryPath = None
+def printList(list: [any]) -> None:
+    for value in list:
+        print(value)
+
+def getAllPathsFromDirectory(directory : Path, recursive : bool, paths: [str] = [], ) -> [str]:
+    directories = []
+    for file in directory.iterdir():
+        if file.is_dir():
+            if recursive:
+                directories.append(file)
+        else:
+            paths.append(str(file))
+    if recursive:
+        for file in directories:
+            getAllPathsFromDirectory(file,recursive,paths)
+    return paths
+
+def readFirstLine() -> (str,Path):
     while True:
         inputLine = input()
         if len (inputLine) < 3:
@@ -15,18 +31,21 @@ def digInTheDirt() -> None:
         if not directoryPath.is_dir() or (mode != 'D' and mode != 'R'):
             error()
             continue
-        break
-    if mode == 'D':
+        return (mode,directoryPath)
 
+def readSecondLine() -> (str,any):
+    while True:
+        inputLine = input()
+        mode = inputLine[0:1]
+        if mode == 'A' and len(inputLine) == 1:
+            return ('A',None)
 
-def getAllPathsFromDirectory(directory : Path, recursive : bool, paths: [str] = []) -> [str]:
-    for file in directory.iterdir():
-        if recursive and file.is_dir():
-            getAllPathsFromDirectory(file,recursive,paths)
-        paths.append(file.abspath())
+def digInTheDirt() -> None:
 
-def error() -> None:
-    print('ERROR')
+    mode,path = readFirstLine()
+    paths = getAllPathsFromDirectory(path,mode == 'R')
+    printList(paths)
+    mode,value = readSecondLine()
 
 if __name__ == '__main__':
     digInTheDirt()
